303e6b1c3264a2409f7a19fc82576591530b3d10
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-12-06 20:37:44 +0100

    F S UOPPPUP up

---
+++ src/components/Choice.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export class Choice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 0
+    }
+  }
+
+  render() {
+  const myStyle = {height: this.state.height + "px"};
+    return (
+      <div className="row">
+      <h2>{this.props.ChoiceTitle}</h2>
+        <div className="col-6" >
+          <button onMouseDown={this.props.lessLess} onClick={this.props.addOne} onMouseUp={this.props.stopLess}>LESS</button>
+          <button onMouseDown={this.props.moreMore} onClick={this.props.addOne} onMouseUp={this.props.stopMore}>MORE</button>
+        </div>
+        <div className="col-6">
+          <div style={myStyle} className="filling container" ></div>
+        </div>
+      </div>
+    )
+  };
+}
+
+export default Choice;
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
+import Choice from './Choice'
+
 
 export class Palette extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      height: 0
+      foods:
+     [{food1: "Erdnuesse"},
+      {food2: "Walnuesse"},
+      {food3: "Haselnuesse"},
+      {food4: "Mandeln"}]
     }
   }
-
   render() {
-  const myStyle = {height: this.state.height + "px"};
-    return (
-      <div className="row">
-        <div className="col-6" >
-          <button onMouseDown={this.props.lessLess} onClick={this.props.addOne} onMouseUp={this.props.stopLess}>LESS</button>
-          <button onMouseDown={this.props.moreMore} onClick={this.props.addOne} onMouseUp={this.props.stopMore}>MORE</button>
-        </div>
-        <div className="col-6">
-          <div style={myStyle} className="filling container" ></div>
-        </div>
-      </div>
-    )
+    return this.state.foods.map((food) => (
+      <Choice ChoiceTitle={Object.values(food)[0]} />
+    ));
   };
 }
 
