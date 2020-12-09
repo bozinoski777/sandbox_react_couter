3a123219f0c9438234b5481aa6b9b08c687635b2
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-12-09 11:30:18 +0100

    Image is button i pallette

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -7,10 +7,10 @@ export class Choice extends Component {
 
   render() {
     return (
-      <div className="row">
+      <div className="col-3">
       <h2>{this.props.ChoiceTitle}</h2>
+      <img  onMouseDown={this.props.moreMore} onClick={this.props.addOne} onMouseUp={this.props.stopMore} src={this.props.ChoiceImg} style={{width: '200px'}} alt="" />
         <div className="col-6" >
-          <button onMouseDown={this.props.moreMore} onClick={this.props.addOne} onMouseUp={this.props.stopMore}>MORE</button>
           <button onMouseDown={this.props.lessLess} onClick={this.props.addOne} onMouseUp={this.props.stopLess}>LESS</button>
         </div>
       </div>
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -6,17 +6,19 @@ export class Palette extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      foods: [{food1: "Erdnuesse"},
-              {food2: "Walnuesse"},
-              {food3: "Haselnuesse"},
-              {food4: "Mandeln"}]
+      foods: [{foodName: "Nelken", foodImg: "https://res.cloudinary.com/dj9iphc8u/image/upload/v1607507350/SmoothieJar/Org_N_ejcdac.jpg"},
+              {foodName: "Chiasaamen", foodImg: "https://res.cloudinary.com/dj9iphc8u/image/upload/v1607507349/SmoothieJar/Org_C_icsfcx.jpg"},
+              {foodName: "Kürbisskerne", foodImg: "https://res.cloudinary.com/dj9iphc8u/image/upload/v1607507349/SmoothieJar/Org_P_x9rwrj.jpg"},
+              {foodName: "Hanfsaamen", foodImg: "https://res.cloudinary.com/dj9iphc8u/image/upload/v1607507348/SmoothieJar/Org_H_qf7kdl.jpg"},
+              {foodName: "Flohsaamen", foodImg: "https://res.cloudinary.com/dj9iphc8u/image/upload/v1607507348/SmoothieJar/Org_Floh_ronshw.jpg"},
+              {foodName: "Lainsaamen", foodImg: "https://res.cloudinary.com/dj9iphc8u/image/upload/v1607507363/SmoothieJar/Org_L_awwavh.jpg"}]
     }
   }
 
 
   render() {
     return this.state.foods.map((food) => (
-      <Choice ChoiceTitle={Object.values(food)[0]} moreMore={this.props.moreMore} stopMore={this.props.stopMore} lessLess={this.props.lessLess} stopLess={this.props.stopLess} addOne={this.props.addOne}/>
+      <Choice ChoiceImg={Object.values(food)[1]} ChoiceTitle={Object.values(food)[0]} moreMore={this.props.moreMore} stopMore={this.props.stopMore} lessLess={this.props.lessLess} stopLess={this.props.stopLess} addOne={this.props.addOne}/>
     ));
   };
 }
